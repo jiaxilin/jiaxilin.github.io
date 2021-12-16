cf2ece80cb0fd275ebb5eb329f8a9b9f09200d1c
jiaxiwunderfauks <jiaxi.dev@gmail.com>
2021-12-16 16:06:18 +0800

    Update main.js

--- trakt/js/main.js
+++ trakt/js/main.js
@@ -1,6 +1,45 @@
 $(document).ready(function() {
 	var api_key = '90bc62ae93bc6cdb8d726039ef45cbcb375c4b5c23bb22cf58ca039e96ecb1ef';
 
+	function ajax_director(movie_id) {
+		$.ajax({
+			type: 'GET',
+			url: 'https://api.trakt.tv/movies/' + movie_id + '/people',
+			headers: {
+				"trakt-api-key": api_key,
+				"trakt-api-version": 2
+			},
+			dataType: 'json',
+			success: function(res) {
+				return res.crew.directing[res.crew.directing.findIndex(e => e.job == 'Director')].person.name;
+			},
+			error: function(err) {
+				return err;
+			}
+		});
+	}
+
+	function ajax_runtime(movie_id) {
+		$.ajax({
+			type: 'GET',
+			url: 'https://api.trakt.tv/movies/' + movie_id,
+			headers: {
+				"trakt-api-key": api_key,
+				"trakt-api-version": 2
+			},
+			dataType: 'json',
+			data: {
+				extended: 'full'
+			},
+			success: function(res) {
+				return res.runtime;
+			},
+			error: function(err) {
+				return err;
+			}
+		});
+	}
+
 	$('#load-data').on('click', function() {
 		$.ajax({
 			type: 'GET',
@@ -15,42 +54,9 @@ $(document).ready(function() {
 					var trakt_movie_id = e.movie.ids.trakt;
 					var director, runtime;
 
-					$.ajax({
-						type: 'GET',
-						url: 'https://api.trakt.tv/movies/' + trakt_movie_id + '/people',
-						headers: {
-							"trakt-api-key": api_key,
-							"trakt-api-version": 2
-						},
-						dataType: 'json',
-						success: function(res) {
-							// director = res.crew.directing[res.crew.directing.findIndex(e => e.job == 'Director')].person.name;
-							console.log(res);
-						},
-						error: function(err) {
-							console.log(err);
-						}
-					});
-
-					$.ajax({
-						type: 'GET',
-						url: 'https://api.trakt.tv/movies/' + trakt_movie_id,
-						headers: {
-							"trakt-api-key": api_key,
-							"trakt-api-version": 2
-						},
-						dataType: 'json',
-						data: {
-							extended: 'full'
-						},
-						success: function(res) {
-							// runtime = res.runtime;
-							console.log(res);
-						},
-						error: function(err) {
-							console.log(err);
-						}
-					});
+					$.when(ajax_director(trakt_movie_id), ajax_runtime(trakt_movie_id)).done(function(a1, a2) {
+						console.log(a1, a2);
+					})
 
 					$("#watchlist tbody").empty().append(`
 						<tr>
