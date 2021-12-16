0020893f264b2e47ee2e10c6919b941c2f098faa
jiaxiwunderfauks <jiaxi.dev@gmail.com>
2021-12-16 17:18:47 +0800

    Fixed extra data bug

--- trakt/js/main.js
+++ trakt/js/main.js
@@ -2,25 +2,19 @@ $(document).ready(function() {
 	var api_key = '90bc62ae93bc6cdb8d726039ef45cbcb375c4b5c23bb22cf58ca039e96ecb1ef';
 
 	function ajax_director(movie_id) {
-		$.ajax({
+		return $.ajax({
 			type: 'GET',
 			url: 'https://api.trakt.tv/movies/' + movie_id + '/people',
 			headers: {
 				"trakt-api-key": api_key,
 				"trakt-api-version": 2
 			},
-			dataType: 'json',
-			success: function(res) {
-				return res.crew.directing[res.crew.directing.findIndex(e => e.job == 'Director')].person.name;
-			},
-			error: function(err) {
-				return err;
-			}
+			dataType: 'json'
 		});
 	}
 
 	function ajax_runtime(movie_id) {
-		$.ajax({
+		return $.ajax({
 			type: 'GET',
 			url: 'https://api.trakt.tv/movies/' + movie_id,
 			headers: {
@@ -30,17 +24,13 @@ $(document).ready(function() {
 			dataType: 'json',
 			data: {
 				extended: 'full'
-			},
-			success: function(res) {
-				return res.runtime;
-			},
-			error: function(err) {
-				return err;
 			}
 		});
 	}
 
 	$('#load-data').on('click', function() {
+		$("#watchlist tbody").empty();
+
 		$.ajax({
 			type: 'GET',
 			url: 'https://api.trakt.tv/users/loljiaxi/watchlist/movies/added',
@@ -55,18 +45,19 @@ $(document).ready(function() {
 					var director, runtime;
 
 					$.when(ajax_director(trakt_movie_id), ajax_runtime(trakt_movie_id)).done(function(a1, a2) {
-						console.log(a1, a2);
-					})
-
-					$("#watchlist tbody").empty().append(`
+						director = a1[0].crew.directing[a1[0].crew.directing.findIndex(e => e.job.includes('Director', 0))].person.name;
+						runtime = a2[0].runtime;
+						
+						$("#watchlist tbody").append(`
 						<tr>
 							<td>${e.movie.title}</td>
 							<td>${e.movie.year}</td>
 							<td>${director}</td>
 							<td>${runtime}</td>
 						</tr>
-					`);
-				})
+						`);
+					});
+				});
 			},
 			error: function(err) {
 				console.log(err);
