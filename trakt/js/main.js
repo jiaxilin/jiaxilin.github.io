88c84391733c6b91320b4f7cb3b2e5ed14fb5407
jiaxiwunderfauks <jiaxi.dev@gmail.com>
2021-12-17 13:21:50 +0800

    List order inconsistent bug fixed

--- trakt/js/main.js
+++ trakt/js/main.js
@@ -42,25 +42,32 @@ $(document).ready(function() {
 			success: function(res) {
 				$.each(res, function(i, e) {
 					var trakt_movie_id = e.movie.ids.trakt;
-					var director, runtime;
 
-					$.when(ajax_director(trakt_movie_id), ajax_runtime(trakt_movie_id)).done(function(a1, a2) {
-						director = a1[0].crew.directing[a1[0].crew.directing.findIndex(e => e.jobs.indexOf('Director') > -1)].person.name;
-						runtime = a2[0].runtime;
-						
-						$("#watchlist tbody").append(`
-						<tr>
-							<td>${e.movie.title}</td>
-							<td>${e.movie.year}</td>
-							<td>${director}</td>
-							<td>${runtime}</td>
-						</tr>
-						`);
-					});
+					$("#watchlist tbody").append(`
+					<tr data-trakt-index="${trakt_movie_id}">
+						<td id="title">${e.movie.title}</td>
+						<td id="year">${e.movie.year}</td>
+						<td id="director"></td>
+						<td id="runtime"></td>
+					</tr>
+					`);
 				});
 			},
 			error: function(err) {
 				console.log(err);
+			},
+			complete: function() {
+				$.each($('#watchlist tbody tr'), function(i, e) {
+					var trakt_movie_id = $(e).data('trakt-index');
+
+					$.when(ajax_director(trakt_movie_id), ajax_runtime(trakt_movie_id)).done(function(a1, a2) {
+						var director = a1[0].crew.directing[a1[0].crew.directing.findIndex(e => e.jobs.indexOf('Director') > -1)].person.name;
+						var runtime = a2[0].runtime;
+
+						$(e).find('td#director').html(director);
+						$(e).find('td#runtime').html(runtime);
+					});
+				});
 			}
 		});
 	});
